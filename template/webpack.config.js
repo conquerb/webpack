28e72019f8e3e2581ab83352b7c405fbb665c32e
yangdh <406002906@qq.com>
2018-03-21 17:37:06 +0800

    修正vue引用路径错误

--- template/webpack.config.js
+++ template/webpack.config.js
@@ -80,7 +80,7 @@ module.exports = {
     },
     resolve: {
         alias: {
-            'vue$': 'vue/build/vue.esm.js'
+            'vue$': 'vue/dist/vue.esm.js'
         },
         extensions: ['*', '.js', '.vue', '.json']
     },
